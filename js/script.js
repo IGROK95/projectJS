9f84f3207d4796f1db0b00567e0a32f64f1dadef
Abu <igrok95_13@mail.ru>
2021-06-25 19:04:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+"use strick";
+
+
+let number = 4;
+const leftBorderWidth = 1;
+console.log(2*4 == 8);
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked || !isClose);
+
+console.log(isChecked || isClose);
