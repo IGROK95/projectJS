a3ede5a8a2e35348cef8a90ed45b363c741cdd8a
Abu <igrok95_13@mail.ru>
2021-06-27 16:18:53 +0300

    add first code in App

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-
 "use strick";
 
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?', '');
 
-let number = 4;
-const leftBorderWidth = 1;
-console.log(2*4 == 8);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
-const isChecked = true,
-  isClose = false;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(isChecked || !isClose);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(isChecked || isClose);
+console.log(personalMovieDB);
